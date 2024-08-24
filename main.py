a61239f555c2b868d1e1c335bef77037dbc1f811
terryproctor <terryproctor2015@outlook.com>
2024-08-24 23:09:09 +0100

    final functional app

--- main.py
+++ main.py
@@ -7,24 +7,31 @@ GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
 WORK_MIN = 25
-SHORT_BREAK_MIN = 1
+SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
-marks = ""
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 def reset():
-    pass
+    global reps
+    reps = 0
+    window.after_cancel(timer)
+    label.config(text="Timer", fg=GREEN)
+    canvas.itemconfig(timer_text, text= "00:00")
+    check_mark.config(text="")
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
     global reps 
-    global marks
+
     reps += 1
 
     if(reps % 2 == 0):
-        marks += "✅"
-        print(reps, marks)
+        marks=""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✅"
         check_mark.config(text=marks)
 
     if(reps % 2 != 0):
@@ -47,6 +54,7 @@ def start_timer():
 def count_down(count):
     min = math.floor(count / 60)
     sec = count % 60
+    global timer
 
     if min == 0:
         min = "00"
@@ -60,7 +68,7 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text= f"{min}:{sec}")
     if count > 0:
-        window.after(1000, count_down, count -1)
+        timer = window.after(1000, count_down, count -1)
     else:
         start_timer()
 
